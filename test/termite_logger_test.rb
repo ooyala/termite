f98c759e099beefcce675f3192d7a7a0eed349d7
Noah Gibbs <noah@ooyala.com>
2011-09-02 13:20:16 -0700

    New tests for logging functions

--- test/termite_logger_test.rb
+++ test/termite_logger_test.rb
@@ -18,9 +18,10 @@ ECOLOGY_TEXT
       File.expects(:read).with("/tmp/bob.ecology").returns(ecology_text)
     end
 
-    context "and only default logging levels set" do
+    context "and fully permissive logging levels set" do
       setup do
         @logger = Termite::Logger.new
+        @logger.level = Logger::DEBUG
       end
 
       should "correctly send logs to Syslog" do
@@ -36,6 +37,26 @@ ECOLOGY_TEXT
       should "correctly send a critical event to Syslog" do
         Termite::Logger::SYSLOG.expects(:crit).with("[main]: foo! {}")
         @logger.fatal("foo!")
+      end
+
+      should "correctly send an error event to Syslog" do
+        Termite::Logger::SYSLOG.expects(:err).with("[main]: foo! {}")
+        @logger.error("foo!")
+      end
+
+      should "correctly send a warning event to Syslog" do
+        Termite::Logger::SYSLOG.expects(:warn).with("[main]: foo! {}")
+        @logger.warn("foo!")
+      end
+
+      should "correctly send an info event to Syslog" do
+        Termite::Logger::SYSLOG.expects(:info).with("[main]: foo! {}")
+        @logger.info("foo!")
+      end
+
+      should "correctly send a debug event to Syslog" do
+        Termite::Logger::SYSLOG.expects(:debug).with("[main]: foo! {}")
+        @logger.debug("foo!")
       end
     end
   end
