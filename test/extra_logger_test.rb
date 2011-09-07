18d24dd06bccb8cbae1d3918f32164e5495d6365
Noah Gibbs <noah@ooyala.com>
2011-09-07 15:50:22 -0700

    Add test for extra loggers

---
+++ test/extra_logger_test.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), "test_helper.rb")
+
+class TermiteExtraLoggerTest < Scope::TestCase
+  context "with termite" do
+    setup do
+      $0 = "MyApp"
+      Ecology.reset
+    end
+
+    context "and two extra loggers added" do
+      setup do
+        @logger = Termite::Logger.new("/tmp/test_log_output.txt")  # Test with output file
+        @logger.level = Logger::DEBUG
+        @mock_logger_1 = mock()
+        @logger.add_logger(@mock_logger_1)
+        @logger.add_logger(@mock_logger_2)
+      end
+
+      should "correctly send logs to Syslog" do
+        UDPSocket.expects(:send)
+        @mock_logger_1.expects(:fatal)
+        @mock_logger_2.expects(:fatal)
+        @logger.add(Logger::FATAL, "foo!", {})
+      end
+    end
+  end
+end
